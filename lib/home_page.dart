cb345fc3064df8b3498da7b256cf85fc3d840197
Javier Monasterio - PC Cibernos <jmonasterio@cibernos.com>
2024-10-13 21:04:49 +0200

    Añadido navbar en el footer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,99 +2,139 @@ import 'package:flutter/material.dart';
 import 'vehicle_type_list.dart'; // Importa la página donde listarás los tipos de vehículos
 import 'camera_page.dart'; // Importa la página de la cámara
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String token;
 
   const HomePage({Key? key, required this.token}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    // Puedes cambiar la lógica de navegación según el índice seleccionado
+    if (index == 1) {
+      // Lógica para el botón "Administrar" (icono central)
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => VehicleTypeListPage(token: widget.token),
+        ),
+      );
+    } else if (index == 2) {
+      // Lógica para el botón "Cuenta" (icono derecho)
+      // Navegar o mostrar una pantalla de perfil, por ejemplo
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        centerTitle: true,
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.dark().copyWith(
+          primary: Colors.blueAccent,
+        ),
+        scaffoldBackgroundColor: Colors.black87,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            backgroundColor: Colors.blueAccent, // Color de fondo del botón
+            foregroundColor: Colors.white, // Color del texto
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+        textTheme: TextTheme(
+          titleLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+          bodyLarge: TextStyle(fontSize: 18, color: Colors.white),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home', style: Theme.of(context).textTheme.titleLarge),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch, // Para que los botones tengan el mismo ancho
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => VehicleTypeListPage(token: widget.token),
+                    ),
+                  );
+                },
+                child: Text('Gestionar tipos de vehículos', style: TextStyle(fontSize: 18)),
               ),
-              onPressed: () {
-                // Llama directamente al fetch para cargar los tipos de vehículos
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => VehicleTypeListPage(token: token),
-                  ),
-                );
-              },
-              child: Text('Gestionar tipos de vehículos'),
+              SizedBox(height: 20), // Espaciado entre botones
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para gestionar marcas
+                },
+                child: Text('Gestionar marcas', style: TextStyle(fontSize: 18)),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para gestionar modelos
+                },
+                child: Text('Gestionar modelos', style: TextStyle(fontSize: 18)),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para gestionar vehículos
+                },
+                child: Text('Gestionar vehículos', style: TextStyle(fontSize: 18)),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CameraPage(token: widget.token),
+                    ),
+                  );
+                },
+                child: Text('Añadir nuevo vehículo', style: TextStyle(fontSize: 18)),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Inicio',
             ),
-            SizedBox(height: 20), // Añadido para espaciado
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              onPressed: () {
-                // Acción para gestionar marcas
-              },
-              child: Text('Gestionar marcas'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Administrar',
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              onPressed: () {
-                // Acción para gestionar modelos
-              },
-              child: Text('Gestionar modelos'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              onPressed: () {
-                // Acción para gestionar vehículos
-              },
-              child: Text('Gestionar vehículos'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              onPressed: () {
-                // Navega a la página para añadir un nuevo vehículo (usar la cámara)
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CameraPage(token: token),
-                  ),
-                );
-              },
-              child: Text('Añadir nuevo vehículo'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Cuenta',
             ),
           ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blueAccent,
+          unselectedItemColor: Colors.white70,
+          backgroundColor: Colors.black87,
+          onTap: _onItemTapped,
         ),
       ),
     );
